4b7259ccf6c741838cb88a0aa8dc2b138430fa43
Edouard DUPIN <yui.heero@gmail.com>
2015-10-14 21:21:03 +0200

    [DEV] update new lutin 0.8.0

--- lutin_audio-algo-drain-test.py
+++ lutin_audio-algo-drain-test.py
@@ -3,17 +3,34 @@ import lutin.module as module
 import lutin.tools as tools
 import lutin.debug as debug
 
+
+def get_type():
+	return "BINARY"
+
+def get_sub_type():
+	return "TEST"
+
 def get_desc():
 	return "test drain audio algo"
 
+def get_licence():
+	return "APACHE-2"
 
-def create(target):
-	my_module = module.Module(__file__, 'audio-algo-drain-test', 'BINARY')
+def get_compagny_type():
+	return "com"
+
+def get_compagny_name():
+	return "atria-soft"
+
+def get_maintainer():
+	return ["Mr DUPIN Edouard <yui.heero@gmail.com>"]
+
+def create(target, module_name):
+	my_module = module.Module(__file__, module_name, get_type())
 	my_module.add_src_file([
-		'test/main.cpp',
-		'test/debug.cpp'
+		'test/main.cpp'
 		])
-	my_module.add_module_depend(['audio-algo-drain'])
+	my_module.add_module_depend(['audio-algo-drain', 'test-debug'])
 	return my_module
 
 
--- lutin_audio-algo-drain.py
+++ lutin_audio-algo-drain.py
@@ -3,16 +3,31 @@ import lutin.module as module
 import lutin.tools as tools
 import lutin.debug as debug
 
+
+def get_type():
+	return "LIBRARY"
+
 def get_desc():
 	return "drain basic algorithm"
 
 def get_licence():
-	# return a table with : "Licence Name", contamination if link static, contamination if link dynamic, "Licence string description / FileName / auto for classicle licence"]
-	return ["APACHE-2", False, False, "auto"]
+	return "APACHE-2"
 
 
-def create(target):
-	my_module = module.Module(__file__, 'audio-algo-drain', 'LIBRARY')
+def get_compagny_type():
+	return "com"
+
+def get_compagny_name():
+	return "atria-soft"
+
+def get_maintainer():
+	return ["Mr DUPIN Edouard <yui.heero@gmail.com>"]
+
+def get_version():
+	return [0,0,0]
+
+def create(target, module_name):
+	my_module = module.Module(__file__, module_name, get_type())
 	my_module.add_src_file([
 		'audio/algo/drain/debug.cpp',
 		'audio/algo/drain/BiQuad.cpp',
@@ -25,11 +40,7 @@ def create(target):
 		'audio/algo/drain/Equalizer.h'
 		])
 	my_module.add_module_depend(['etk', 'audio'])
-	
-	my_module.add_export_path(tools.get_current_path(__file__))
-	
-	#my_module.set_licence(module.APACHE_2)
-	
+	my_module.add_path(tools.get_current_path(__file__))
 	return my_module
 
 
